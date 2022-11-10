557c9403f36866e04d12db3bc7869d5b7e0d88a3
EvgeniaKozlova <photofridzon@gmail.com>
2022-11-10 23:28:21 +0700

    Создана функция Counter

--- FinalTask.cs
+++ FinalTask.cs
@@ -2,3 +2,15 @@
 string[] str2 = new string[str.Length]; 
 
 
+void Counter(string[] str, string[] str2)
+{
+    int count = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        if(str[i].Length <= 3)
+        {
+            str2[count] = str[i];
+            count++;
+        }
+    }
+}
